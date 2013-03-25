e30d1e24afaaf3c6379530445bc9888b0c2921d5
Mickaël Salaün <mic@digikod.net>
2013-03-26 00:12:41 +0100

    verify: Simplify arguments and be more verbose
    arg0: git directory (or local by default)
    Only use the HEAD commit

--- verify.sh
+++ verify.sh
@@ -2,23 +2,27 @@
 
 usage() {
 	echo "Simple script to verify a grsecurity signature" >&2
-	echo "usage: $0 <grsecurity-version> [linux-git-directory]" >&2
+	echo "usage: $0 [linux-git-directory]" >&2
 	exit 1
 }
 
-GRSEC_VERSION="$1"
-GIT_DIR="$2"
-LINUX_VERSION="$(echo "${GRSEC_VERSION}" | sed -nr 's/[.0-9]+-([.0-9]+)-[0-9]+/\1/p')"
-
-if [ -z "${LINUX_VERSION}" ]; then
-	usage
-fi
+GIT_DIR="${1:-.}"
 if ! cd "${GIT_DIR}"; then
 	usage
 fi
 if [ ! -d ".git" ]; then
 	usage
 fi
+
+GRSEC_PATCH="$(git cat-file commit HEAD | awk 'NR==6 && $1=="Import" { print $2 }')"
+LINUX_VERSION="$(echo "${GRSEC_PATCH}" | sed -nr 's/^grsecurity-[.0-9]+-([.0-9]+)-[0-9]+\.patch$/\1/p')"
+
+if [ -z "${LINUX_VERSION}" ]; then
+	usage
+fi
+
+echo "Patch: ${GRSEC_PATCH}"
+echo "Linux: v${LINUX_VERSION}"
 
 TMP="$(mktemp 2>/dev/null || echo ./grsec.patch)"
 cleanup() {
